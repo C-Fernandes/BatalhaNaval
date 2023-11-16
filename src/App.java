9e052259c5c694ba148d2a8bad4a3a036717a9bd
Maria Clara Fernandes <66803310+C-Fernandes@users.noreply.github.com>
2023-11-15 21:12:59 -0300

    JavaFX funcional (pelo menos até o momento)

--- src/App.java
+++ src/App.java
@@ -1,32 +1,40 @@
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
-import javafx.scene.media.MediaView;
 import javafx.stage.Stage;
-
-public class App  extends Application{
-    public static void main(String[] args) throws Exception {
-        launch(args);
-    }
-
+ 
+public class App extends Application {
     @Override
-    public void start(Stage primeiroEstagio) throws Exception {
-        StackPane raiz = new StackPane();
-        Label label = new Label();
-        FXMLLoader fxml = new FXMLLoader(getClass().getResource("tela_inicial.fxml"));
-
-        Parent root = fxml.load();
-        Scene telaInicial = new Scene(root);
-
-        primeiroEstagio.setTitle("Batalha Naval");
-        primeiroEstagio.setScene(telaInicial);;
-        primeiroEstagio.show();
-
-        throw new UnsupportedOperationException("Unimplemented method 'start'");
+    public void start(Stage primaryStage) {
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+ 
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+  
+  /*
+  Parent root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
+  Scene scene = new Scene(root);
+  */
+  
+  Scene scene = new Scene(root, 300, 250);
+  
+  primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+ 
+ public static void main(String[] args) {
+        launch(args);
     }
 }
